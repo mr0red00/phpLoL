040bb6d4b6ad2d59fdfdcd6b02c6cfe15666a8dd
david <zlokomatic@gmail.com>
2013-09-25 21:05:04 +0200

    Example for getAllLeaguesForPlayer

--- src/zlokomatic/phpLoL/examples/index.php
+++ src/zlokomatic/phpLoL/examples/index.php
@@ -6,3 +6,12 @@ use zlokomatic\phpLoL\LoLClient;
 
 $client = new LoLClient('Username', 'Password', 'NA' );
 var_dump($client->getAggregatedStats(12345, 'CLASSIC', 'CURRENT'));
+
+$summonerId = 12345;
+$leagues = $client->getAllLeaguesForPlayer($summonerId);
+$leagues = $leagues["summonerLeagues"];
+
+foreach($leagues as $leagueArray){
+    $leagueArray = $leagueArray->getAmfData();
+    echo "League: " . $leagueArray['queue'] . " - " . $leagueArray['name'] . " - " . $leagueArray['tier'] . " - " . $leagueArray['requestorsRank'] . "\n";
+}
